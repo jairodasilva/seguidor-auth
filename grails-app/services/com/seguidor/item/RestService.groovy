b7f1ac5feb9387286ca17aeeeb3c23744d711024
Camilo Verdugo <camilo.verdugo@mercadolibre.cl>
2016-02-26 15:28:54 -0300

    fix charset

--- grails-app/services/com/seguidor/item/RestService.groovy
+++ grails-app/services/com/seguidor/item/RestService.groovy
@@ -14,6 +14,7 @@ import org.grails.web.json.JSONObject
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.StringHttpMessageConverter
+import java.nio.charset.Charset
 
 /**
  * Common service options
